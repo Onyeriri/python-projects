ee137e5d58cc1fb9203d4dd8dea54f007fcd9a36
Onyeriri Victor <viktorchinonso@gmail.com>
2023-01-24 11:08:12 +0000

    Basic string types

--- python-basis/main.py
+++ python-basis/main.py
@@ -6,4 +6,9 @@ num3 = 9.0
 typeOf = type(num1)
 typeOf1 = type(num3)
 
-print(typeOf1)
+# basic type string
+string1 = 'She\'s happy doing her chore\'s'
+string2 = "Hello string"
+
+
+print(string2)
